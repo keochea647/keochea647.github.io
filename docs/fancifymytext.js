0023db154130eb245064a8152aea061b2231d348
Keochea <ksun8900@gmail.com>
2022-10-26 23:22:28 -0700

    Assignment 4 completion

--- docs/fancifymytext.js
+++ docs/fancifymytext.js
@@ -1,1 +1,45 @@
-alert("Hello, world!");
+const TXT = document.getElementById('text');
+const mooBtn = document.getElementById('moo');
+
+let fancyAlert = true;
+let greetingAlert = true;
+
+function bigger() {
+    if(greetingAlert) {
+        alert("Hello, world!");
+        greetingAlert=false;
+    }
+    TXT.style.fontSize = "24pt";
+};
+function resetStyle() {
+    TXT.style.fontSize = null;
+    TXT.style.fontWeight = null;
+    TXT.style.color = null;
+    TXT.style.textDecoration = null;
+};
+function fancify() {
+    if(fancyAlert) {
+        alert('FANCIFY!');
+        fancyAlert=false;
+    }
+    TXT.style.fontWeight = 'bold';
+    TXT.style.textDecoration = 'underline';
+    TXT.style.color='blue';
+};
+function radioFunc(caller) {
+    if(caller.id == 'fancy') {
+        fancify();
+        document.getElementById('boring').checked=false;
+    }
+    else {
+        resetStyle();
+        document.getElementById('fancy').checked=false;
+    }
+}
+function moo() {
+    let str = TXT.value.toUpperCase();
+    let parts = str.split(".");
+    str = parts.join("-Moo");
+    TXT.value = str;
+}
+mooBtn.addEventListener('click',moo,false);
