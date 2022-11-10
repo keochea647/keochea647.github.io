03a0b2af0b3e94a8f8c20007f8e9ca3fe7446902
Keochea <ksun8900@gmail.com>
2022-11-10 09:29:35 -0800

    Fixing my assignment 4

--- docs/fancifymytext.js
+++ docs/fancifymytext.js
@@ -1,45 +1,43 @@
-const TXT = document.getElementById('text');
-const mooBtn = document.getElementById('moo');
-
-let fancyAlert = true;
-let greetingAlert = true;
-
+let fancyAlert=true;
+let greetingAlert=true;
+    
 function bigger() {
+    let txt=document.getElementById("text");
     if(greetingAlert) {
         alert("Hello, world! (one time alert)");
         greetingAlert=false;
     }
-    TXT.style.fontSize = "24pt";
+    txt.style.fontSize = "24pt";
 };
 function resetStyle() {
-    TXT.style.fontSize = null;
-    TXT.style.fontWeight = null;
-    TXT.style.color = null;
-    TXT.style.textDecoration = null;
+    let txt=document.getElementById("text");
+    txt.style.fontSize = null;
+    txt.style.fontWeight = null;
+    txt.style.color = null;
+    txt.style.textDecoration = null;
 };
 function fancify() {
+    let txt=document.getElementById("text");
     if(fancyAlert) {
         alert('FANCIFY! (one time alert)');
         fancyAlert=false;
     }
-    TXT.style.fontWeight = 'bold';
-    TXT.style.textDecoration = 'underline';
-    TXT.style.color='blue';
+    txt.style.fontWeight = 'bold';
+    txt.style.textDecoration = 'underline';
+    txt.style.color='blue';
 };
 function radioFunc(caller) {
     if(caller.id == 'fancy') {
         fancify();
-        document.getElementById('boring').checked=false;
     }
     else {
         resetStyle();
-        document.getElementById('fancy').checked=false;
     }
 }
-function moo() {
-    let str = TXT.value.toUpperCase();
+function mooFunc() {
+    let txt=document.getElementById("text");
+    let str = txt.value.toUpperCase();
     let parts = str.split(".");
     str = parts.join("-Moo");
-    TXT.value = str;
+    txt.value = str;
 }
-mooBtn.addEventListener('click',moo,false);
